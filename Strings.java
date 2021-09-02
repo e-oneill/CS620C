9de44b4798bb4832189db8b6bca4e91059feccd9
Eoin O'Neill <EOIN.ONEILL.2022@MUMAIL.IE>
2021-09-02 09:58:09 +0100

    wednesday morning

---
+++ Strings.java
@@ -0,0 +1,14 @@
+public class Strings
+{
+    public static void main(String[] args) {
+        String s1 = "10", s2 = "12", s3 = s1 + " " + s2;
+        int num1 = 10, num2 = 12;
+
+        System.out.println(s1 + s2);
+        
+        System.out.println(num1 + num2);
+
+        System.out.println(s1 + s2 + num1 + num2);
+
+    }
+}
