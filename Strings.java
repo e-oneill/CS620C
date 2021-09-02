c62b02312332f5aac14df232a6c59b58d943e034
Eoin O'Neill <EOIN.ONEILL.2022@MUMAIL.IE>
2021-09-02 11:46:33 +0100

    wednesday afternoon

--- Strings.java
+++ Strings.java
@@ -1,14 +1,28 @@
 public class Strings
 {
     public static void main(String[] args) {
-        String s1 = "10", s2 = "12", s3 = s1 + " " + s2;
-        int num1 = 10, num2 = 12;
+        String s1 = "Mule"; 
+        String s2 = "Mule", s3 = s1 + " " + s2;
+        // int num1 = 10, num2 = 12;
 
-        System.out.println(s1 + s2);
+        // System.out.println(s1 + s2);
         
-        System.out.println(num1 + num2);
+        // System.out.println(num1 + num2);
 
-        System.out.println(s1 + s2 + num1 + num2);
+        // System.out.println(s1 + s2 + num1 + num2);
+
+        // System.out.println(s1==s2);
+        // System.out.println(s1.equals(s2));
+
+        String phnumber = "353831871363";
+
+        String countrycode = phnumber.substring(0, 3);
+        String operator = phnumber.substring(3,5);
+        String number = phnumber.substring(5);
+
+        System.out.println(countrycode);
+        System.out.println(operator);
+        System.out.println(number);
 
     }
 }
