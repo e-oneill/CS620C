6f46a7ed8547b99d6bc184c54da3c84e5f65a39c
Eoin O'Neill <EOIN.ONEILL.2022@MUMAIL.IE>
2021-09-13 13:34:36 +0100

    Monday Labs

---
+++ Week_3/TestBasicCircle.java
@@ -0,0 +1,37 @@
+package Week_3;
+import java.util.*;
+public class TestBasicCircle {
+    public static void main(String[] args) {
+        Scanner sInt = new Scanner(System.in);
+
+        BasicCircle[] circles = new BasicCircle [3];
+        
+        for (int i = 0; i < circles.length; i++)
+        {
+            int radius = sInt.nextInt();
+            circles[i] = new BasicCircle();
+            circles[i].setRadius(radius);
+        }
+
+        sInt.close();
+
+        for (int i = 0; i < circles.length; i++)
+        {
+            int radius = circles[i].getRadius();
+            System.out.println("Circle " + (i+1) + " has a radius of " + radius);
+        }
+
+        for (int i = 0; i < circles.length; i++)
+        {
+            double circumference = circles[i].getCircumference();
+            System.out.println("Circle " + (i+1) + " has a circumference of " + circumference);
+        }
+
+        for (int i = 0; i < circles.length; i++)
+        {
+            double area = circles[i].getArea();
+            System.out.println("Circle " + (i+1) + " has an area of " + area);
+        }
+
+    }
+}
